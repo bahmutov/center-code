bd4abc313b074cba0b6c88ed73c2c3f8c0eba2c8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-09-15 08:51:38 -0400

    computed size of the source

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ function terminalSize() {
   };
 }
 
+function getSource(filename) {
+  var read = require('fs').readFileSync;
+  return read(filename, 'utf-8');
+}
+
+function widest(lines) {
+  return lines.reduce(function (columns, line) {
+    return columns > line.length ? columns : line.length;
+  }, 0);
+}
+
 function centerCode(options) {
   options = options || {};
   options.filename = options.filename || options.name;
   log('showing in the center %s', options.filename);
   var size = terminalSize();
   log('terminal %d x %d', size.width, size.height);
+  var source = getSource(options.filename);
+  var lines = source.split('\n');
+  var columns = widest(lines);
+  log('source size %d x %d', columns, lines.length);
 }
 
 module.exports = centerCode;
