74f3b7d1f7e8dd142d08923c5b12236b4f9b9572
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-09-15 09:09:22 -0400

    padding horizontally and vertically

--- bin/center-code.js
+++ bin/center-code.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+if (process.argv.length < 3) {
+  console.error('center <filename>');
+  process.exit(-1);
+}
+var filename = process.argv[process.argv.length - 1];
 var centerCode = require('..');
+centerCode({ filename: filename });
