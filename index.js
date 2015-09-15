44fb1e8b809b953971fb235bc1272f34ad5906d9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-09-15 08:46:34 -0400

    finding terminal size

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var log = require('debug')('center');
 
+function terminalSize() {
+  return {
+    width: process.stdout.columns,
+    height: process.stdout.rows
+  };
+}
+
 function centerCode(options) {
   options = options || {};
   options.filename = options.filename || options.name;
   log('showing in the center %s', options.filename);
+  var size = terminalSize();
+  log('terminal %d x %d', size.width, size.height);
 }
 
 module.exports = centerCode;
